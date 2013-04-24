c29031751830691f086ee8ba930242938c338a69
lancy <lancy1014@gmail.com>
2013-04-24 13:55:05 +0800

    Initial Commit

---
+++ RSADemo/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  RSADemo
+//
+//  Created by Lancy on 24/4/13.
+//  Copyright (c) 2013 GraceLancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RSADemo/GLViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLViewController.h
+//  RSADemo
+//
+//  Created by Lancy on 24/4/13.
+//  Copyright (c) 2013 GraceLancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLViewController : UIViewController
+
+@end
